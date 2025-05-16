c2a639663092952dfd45ba3f8dc95c9b3643e738
Ben balfas <100008835+BenBalfas@users.noreply.github.com>
2025-05-16 22:51:04 +0700

    Add service, make section skills more efficient, update data skills, service, cv and about

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,24 +1,22 @@
-import React from "react";
-
 const Info = () => {
   return (
     <div className="about__info grid">
       <div className="about__box">
-        <i class="bx bx-award about__icon"></i>
+        <i className="bx bx-award about__icon"></i>
 
         <h3 className="about__title">Experience</h3>
-        <span className="about__title">2 Years Coding</span>
+        <span className="about__title">4 Years Coding</span>
       </div>
 
       <div className="about__box">
-        <i class="bx bx-briefcase-alt about__icon"></i>
+        <i className="bx bx-briefcase-alt about__icon"></i>
 
         <h3 className="about__title">Completed</h3>
         <span className="about__title">3 Projects</span>
       </div>
 
       <div className="about__box">
-        <i class="bx bx-support about__icon"></i>
+        <i className="bx bx-support about__icon"></i>
 
         <h3 className="about__title">Support</h3>
         <span className="about__title">Onilne 24/7</span>
--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -1,67 +1,39 @@
-import React from "react";
+import { frontend } from "./skillList";
 
 const Frontend = () => {
+  const midpoint = Math.ceil(frontend.length / 2);
+  const firstHalf = frontend.slice(0, midpoint);
+  const secondHalf = frontend.slice(midpoint);
+
   return (
     <div className="skills__content">
       <h3 className="skills__title">Frontend Developer</h3>
-
       <div className="skills__box">
         <div className="skills__group">
-          <div className="skills__data">
-            <i class="bx bx-badge-check"></i>
-
-            <div>
-              <h3 className="skills__name">HTML</h3>
-              <span className="skills__level">Advanced</span>
-            </div>
-          </div>
-
-          <div className="skills__data">
-            <i class="bx bx-badge-check"></i>
-
-            <div>
-              <h3 className="skills__name">CSS</h3>
-              <span className="skills__level">Advanced</span>
-            </div>
-          </div>
-
-          <div className="skills__data">
-            <i class="bx bx-badge-check"></i>
-
-            <div>
-              <h3 className="skills__name">JavaScript</h3>
-              <span className="skills__level">Intermediate</span>
-            </div>
-          </div>
+          {firstHalf.map((frontend) => {
+            return (
+              <div key={frontend.id} className="skills__data">
+                <i className="bx bx-badge-check"></i>
+                <div>
+                  <h3 className="skills__name">{frontend.name}</h3>
+                  <span className="skills__level">{frontend.level}</span>
+                </div>
+              </div>
+            );
+          })}
         </div>
-
         <div className="skills__group">
-          <div className="skills__data">
-            <i class="bx bx-badge-check"></i>
-
-            <div>
-              <h3 className="skills__name">Bootstrap</h3>
-              <span className="skills__level">Advanced</span>
-            </div>
-          </div>
-
-          <div className="skills__data">
-            <i class="bx bx-badge-check"></i>
-
-            <div>
-              <h3 className="skills__name">Git</h3>
-              <span className="skills__level">Basic</span>
-            </div>
-          </div>
-
-          <div className="skills__data">
-            <i class="bx bx-badge-check"></i>
-
-            <div>
-              <h3 className="skills__name">React</h3>
-              <span className="skills__level">Intermediate</span>
-            </div>
-          </div>
+          {secondHalf.map((frontend) => {
+            return (
+              <div key={frontend.id} className="skills__data">
+                <i className="bx bx-badge-check"></i>
+                <div>
+                  <h3 className="skills__name">{frontend.name}</h3>
+                  <span className="skills__level">{frontend.level}</span>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
